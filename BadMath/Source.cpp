8574709a98646bf594631864ffc1dcee7f99e26c
Snowy-Cuber <tfischerrenee1@gmail.com>
2025-09-09 23:09:29 -0400

    I fixed 2 errors. One was in the return statement of the average function and the other was getting n2 from the user twice instead of n3. I also made it so that you could calcluate the average of floats not just intergers and changed some spacing.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,25 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(float i1, float i2, float i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
+	float n1 =0 , n2 = 0, n3 = 0;
+
+	cout << "This program calculates the average of three numbers." << endl << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(2) << fixed << "\nThe average is " << a << endl;
 
+	cout << endl << endl;
+	system("pause");
 	return 0;
 }
